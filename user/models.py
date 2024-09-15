390a17667e51db3a46c77687a7d12eb99e61087c
rixyz <rix0ztha@gmail.com>
2024-09-15 16:03:07 +0545

    fix default profile picture

--- user/models.py
+++ user/models.py
@@ -11,7 +11,7 @@ def rename_profile_picture(instance, filename):
 
 class User(AbstractUser):
     bio = models.TextField(max_length=500, null=True, blank=True)
-    profile_picture = models.ImageField(upload_to=rename_profile_picture, default='profile_picture/default_profile.jpg', null=True, blank=True)
+    profile_picture = models.ImageField(upload_to=rename_profile_picture, default='profile_picture/default_profile.jpg')
     date_of_birth = models.DateField(null=True, blank=True)
 
     def __str__(self):
