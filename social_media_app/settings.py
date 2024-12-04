9388123dd01526aa6a5322efa603f8f06aecdf84
Riz <rix0ztha@gmail.com>
2024-12-04 10:41:07 +0545

    docker

--- social_media_app/settings.py
+++ social_media_app/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env("DJANGO_ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
